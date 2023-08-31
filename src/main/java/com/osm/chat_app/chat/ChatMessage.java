fe6c2525faccf690c7ef96996e0a2ed4df2b09e1
Oussama <oussamaait2001@gmail.com>
2023-08-31 16:14:44 +0100

    Completing the backend and index.html, main.css

---
+++ src/main/java/com/osm/chat_app/chat/ChatMessage.java
@@ -0,0 +1,15 @@
+package com.osm.chat_app.chat;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatMessage {
+
+    private String content;
+    private String sender;
+    private MessageType type;
+}
---
+++ src/main/java/com/osm/chat_app/chat/MessageType.java
@@ -0,0 +1,10 @@
+package com.osm.chat_app.chat;
+
+public enum MessageType {
+    // when a user send a message
+    CHAT,
+    // when a user join the message group
+    JOIN,
+    // when a user leaves the message group
+    LEAVE
+}
